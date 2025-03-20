142e39363ed78b4a52167f54bbb748282aeb01d0
Bhukya Prashanth <155879494+chotucoder@users.noreply.github.com>
2025-03-20 22:32:04 +0530

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,5 @@
+function logout() {
+    if (confirm("Are you sure you want to log out?")) {
+        window.location.href = "login.html";
+    }
+}
---
+++ script.js
@@ -0,0 +1,12 @@
+function login() {
+    const userId = document.getElementById("userId").value;
+    const password = document.getElementById("password").value;
+    
+    if (userId === "admin" && password === "admin") {
+        window.location.href = "admin.html";
+    } else if (userId === "user" && password === "user") {
+        window.location.href = "user.html";
+    } else {
+        alert("Invalid credentials");
+    }
+}
