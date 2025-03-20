c25c1baf67d66153836ff721cac13118345237ae
Bhukya Prashanth <155879494+chotucoder@users.noreply.github.com>
2025-03-20 23:00:15 +0530

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-function login() {
+function login(event) {
+    event.preventDefault();
     const userId = document.getElementById("userId").value;
     const password = document.getElementById("password").value;
     
     if (userId === "admin" && password === "admin") {
-        window.location.href = "Admin.html";
+        window.location.href = "admin.html";
     } else if (userId === "user" && password === "user") {
         window.location.href = "user.html";
     } else {
